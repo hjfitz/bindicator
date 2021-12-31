8cfc962f6e42c2057d5a98bd76629d394499911f
harry <hjfitz@protonmail.ch>
2021-12-31 18:05:23 +0000

    add api route for bins

--- src/pages/api/bins.ts
+++ src/pages/api/bins.ts
@@ -1,5 +1,22 @@
 import type {NextApiRequest, NextApiResponse} from 'next'
+import {binServiceFactory} from '../../bins/bins.service'
 
-export default function handler(_: NextApiRequest, res: NextApiResponse<{name: string}>) {
-  res.status(200).json({ name: 'John Doe' })
+const binsService = binServiceFactory()
+
+// todo: create controller class
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'GET') {
+    res.status(405)
+    res.send(`${req.method} not implemented`)
+    return
+  }
+  const url = new URL(req.url)
+  if (url.searchParams.has('m')) {
+    res.status(405)
+    res.send('Not yet implemented by month')
+    return
+  }
+
+  res.json(binsService.findAll())
+
 }
